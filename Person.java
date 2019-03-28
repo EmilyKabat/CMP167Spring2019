fef337949a333c384e7cc29d7998cb12d44a4bbe
EmilyKabat <47428022+EmilyKabat@users.noreply.github.com>
2019-03-28 12:37:59 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Person {
+	
+	String name = "";
+	int age;
+	char gender;
+	
+	public Person(String name, int age, char gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+}
