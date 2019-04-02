b492d9b239b072502be44dc7dbf8f7ce03fc0656
EmilyKabat <47428022+EmilyKabat@users.noreply.github.com>
2019-04-02 12:14:44 -0400

    Add files via upload

---
+++ Calculatte.java
@@ -0,0 +1,20 @@
+package fvx;
+/*
+ * Author: Emily Kabat
+ * Created 4.2.19
+ * This class has calculate methods
+ */
+public class Calculatte {
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public static double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	public static double getRectangleArea(double length, double width) {
+		return width * length;
+	}
+}
